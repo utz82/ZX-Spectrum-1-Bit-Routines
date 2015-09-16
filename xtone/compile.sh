319a0953b19a75bb1acaff0525a431b8bd37f52b
utz82 <ipcore@server.fake>
2015-09-16 21:09:57 +0200

    added missing XM template

--- xtone/compile.sh
+++ xtone/compile.sh
@@ -2,7 +2,7 @@
 
 ./xm2xtone.pl
 #zmakebas -a 10 -o loader.tap loader.bas
-pasmo --alocal --tap main.asm main.tap main.lst
+pasmo --alocal --tap main.asm main.tap #main.lst
 cat loader.tap main.tap > test.tap
 rm main.tap
 #fuse-sdl --no-confirm-actions -m 48 -t test.tap
