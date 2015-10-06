2e0cd2ce099c87e56714950c63ea6ae7b5302d04
utz82 <ipcore@server.fake>
2015-10-06 12:09:28 +0200

    optimized a bit further, player is now 83 bytes

--- nanobeep/compile.sh
+++ nanobeep/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ./xm2nanobeep.pl
-#zmakebas -a 10 -o loader.tap loader.bas
+zmakebas -a 10 -o loader.tap loader.bas
 pasmo -d --alocal --tap main.asm main.tap
 cat loader.tap main.tap > test.tap
 rm main.tap
--- nanobeep/main.asm
+++ nanobeep/main.asm
@@ -1,16 +1,17 @@
 ;******************************************************************
 ;nanobeep
-;86 byte beeper engine by utz 09'2015
+;83 byte beeper engine by utz 09-10'2015
 ;******************************************************************
 ;
 ;ignores kempston
 ;only reads keys Space,A,Q,1 (can be fixed with 2 additional bytes)
 ;
 ;D - add counter ch1
-;IXH - base freq ch1
+;E - base freq ch1
+;B - internal delay counter
 ;C - add counter ch2
-;E - base freq ch2
-;L - timer
+;HL - data pointer
+;IY - timer
 
 	org #8000
 
@@ -22,7 +23,7 @@ init
 ;******************************************************************
 rdseq
 	xor a
-	pop hl			;pattern pointer to DE
+	pop hl			;pattern pointer to HL
 	or h
 	jr nz,rdptn
 	;jr exit		;uncomment to disable looping
@@ -42,23 +43,22 @@ drum
 ;******************************************************************	
 rdptn
 	inc hl	
-	ld a,(hl)
-	ld ixh,a		;base freq ch1
+	ld a,(hl)		;base freq ch1		
+	ld e,a
 	inc a			;if A=#ff
 	jr z,rdseq
 	
 	inc a
 	jr z,drum
 
-	inc hl		
-	ld e,(hl)		;base freq ch2
+	inc hl			;point to base freq ch2	
 
 	ld iy,(musicdata)	;speed
 
 ;******************************************************************
 play
 	ld a,d
-	add a,ixh
+	add a,e
 	ld d,a
 	
 	sbc a,a
@@ -76,7 +76,7 @@ play
 	djnz $
 
 	ld a,c
-	add a,e
+	add a,(hl)
 	ld c,a
 
 	sbc a,a
