59514a2394e0ef88604bfa6e43b46a63aa5e89fe
utz82 <utz82@users.noreply.github.com>
2016-10-19 17:05:19 +0200

    BetaPhase: use "b" instead of "h" as note name

--- betaphase/equates.h
+++ betaphase/equates.h
@@ -22,7 +22,7 @@ rest		equ #0
 
 a0	 equ #1c
 ais0	 equ #1e
-h0	 equ #20
+b0	 equ #20
 c1	 equ #22
 cis1	 equ #24
 d1	 equ #26
@@ -34,7 +34,7 @@ g1	 equ #33
 gis1	 equ #36
 a1	 equ #39
 ais1	 equ #3c
-h1	 equ #40
+b1	 equ #40
 c2	 equ #44
 cis2	 equ #48
 d2	 equ #4c
@@ -46,7 +46,7 @@ g2	 equ #65
 gis2	 equ #6b
 a2	 equ #72
 ais2	 equ #79
-h2	 equ #80
+b2	 equ #80
 c3	 equ #87
 cis3	 equ #8f
 d3	 equ #98
@@ -58,7 +58,7 @@ g3	 equ #cb
 gis3	 equ #d7
 a3	 equ #e4
 ais3	 equ #f1
-h3	 equ #100
+b3	 equ #100
 c4	 equ #10f
 cis4	 equ #11f
 d4	 equ #130
@@ -70,7 +70,7 @@ g4	 equ #196
 gis4	 equ #1ae
 a4	 equ #1c7
 ais4	 equ #1e2
-h4	 equ #1ff
+b4	 equ #1ff
 c5	 equ #21d
 cis5	 equ #23e
 d5	 equ #260
@@ -82,7 +82,7 @@ g5	 equ #32b
 gis5	 equ #35b
 a5	 equ #38f
 ais5	 equ #3c5
-h5	 equ #3fe
+b5	 equ #3fe
 c6	 equ #43b
 cis6	 equ #47b
 d6	 equ #4bf
@@ -94,7 +94,7 @@ g6	 equ #656
 gis6	 equ #6b7
 a6	 equ #71d
 ais6	 equ #789
-h6	 equ #7fc
+b6	 equ #7fc
 c7	 equ #876
 cis7	 equ #8f6
 d7	 equ #97f
