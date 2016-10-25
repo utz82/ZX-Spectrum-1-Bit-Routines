e076971da5de4be039cdcd65729a6be4767baa57
utz82 <utz82@users.noreply.github.com>
2016-10-25 18:26:03 +0200

    PhaserX: Update music.asm example

--- phaserX/music.asm
+++ phaserX/music.asm
@@ -84,26 +84,26 @@ ptn3
 	dw #0800|kick,mix_and|fsid|noupd2,	#0080,rest
 	dw #0800|hhat,mix_and|fsid,		#0080,gis2,	#00c0,#4010,gis3,gis2+8,#4000
 	dw #0800,mix_and|fsid,			#0080,ais2,	#00c0,#4010,ais3,ais2+8,#4000
-	dw #0800|hhat,mix_and|fsid,		#0080,h2,	#00c0,#4010,h3,h2+8,#4000
+	dw #0800|hhat,mix_and|fsid,		#0080,b2,	#00c0,#4010,b3,b2+8,#4000
 	
 	dw #1000|kick,mix_and|fsid,		#0080,f2,	#00c0,#4010,f3,f2+8,#4000
 	dw #0800|kick,mix_and|fsid|noupd2,	#0080,rest
 	dw #0800|hhat,mix_and|fsid,		#0080,gis2,	#00c0,#4010,gis3,gis2+8,#4000
 	dw #0800,mix_and|fsid,			#0080,ais2,	#00c0,#4010,ais3,ais2+8,#4000
-	dw #0800|hhat,mix_and|fsid,		#0080,h2,	#00c0,#4010,h3,h2+8,#4000
+	dw #0800|hhat,mix_and|fsid,		#0080,b2,	#00c0,#4010,b3,b2+8,#4000
 	
 	db ptnend
 	
 ptn4
 	dw #1000|kick,mix_and|fsid,		#0080,g2,	#00c0,#4010,g3,g2+8,#4000
 	dw #0800|kick,mix_and|fsid|noupd2,	#0080,rest
-	dw #0800|hhat,mix_and|fsid,		#0080,h2,	#00c0,#4010,h3,h2+8,#4000
+	dw #0800|hhat,mix_and|fsid,		#0080,h2,	#00c0,#4010,b3,b2+8,#4000
 	dw #0800,mix_and|fsid,			#0080,c3,	#00c0,#4010,c4,c3+8,#4000
 	dw #0800|hhat,mix_and|fsid,		#0080,d3,	#00c0,#4010,d4,d3+8,#4000
 	
 	dw #1000|kick,mix_and|fsid,		#0080,g2,	#00c0,#4010,g3,g2+8,#4000
 	dw #0800|kick,mix_and|fsid|noupd2,	#0080,rest
-	dw #0800|hhat,mix_and|fsid,		#0080,h2,	#00c0,#4010,h3,h2+8,#4000
+	dw #0800|hhat,mix_and|fsid,		#0080,b2,	#00c0,#4010,b3,b2+8,#4000
 	dw #0800,mix_and|fsid,			#0080,c3,	#00c0,#4010,c4,c3+8,#4000
 	dw #0800|hhat,mix_and|fsid,		#0080,d3,	#00c0,#4010,d4,d3+8,#4000
 	
@@ -147,7 +147,7 @@ ptn7
 ptn8
 	dw #0800|kick,mix_and|fnoise,		#0040,#2174,	#00c0,#4010,c3,c2+8,#4000
 	dw #0800,mix_and|fnoise|noupd2,		#0020,#2174
-	dw #0800|kick,mix_xor|fnoise,		#0010,#2174,	#00c0,#4010,h3,h3+8,#4000
+	dw #0800|kick,mix_xor|fnoise,		#0010,#2174,	#00c0,#4010,b3,b3+8,#4000
 	dw #0800|hhat,mix_xor|fnoise|noupd2,	#0040,#2174
 	dw #0800|hhat,mix_xor|fnoise|noupd2,	#0020,#2174
 	dw #0400|hhat,mix_xor|fnoise|noupd2,	#0040,#2174
